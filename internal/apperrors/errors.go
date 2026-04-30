b03f78244ae09763ae30db356bde700634ea41fc
WardenFA <tvinkovich-2000@bk.ru>
2026-04-30 12:55:09 +0300

    CRUD для юзеров + улучшил обработку ошибок и пофиксил баг с DELETE *

--- internal/apperrors/errors.go
+++ internal/apperrors/errors.go
@@ -4,6 +4,7 @@ import "errors"
 
 // кастомные ошибки
 var (
+	ErrNotFound      = errors.New("data not found")
 	ErrUsedEmail     = errors.New("email is already used")
 	ErrForeignKey    = errors.New("foreign key error")
 	ErrNullViolation = errors.New("must be not null")
--- internal/repository/reposotiry.go
+++ internal/repository/reposotiry.go
@@ -7,6 +7,7 @@ import (
 	"errors"
 	"log"
 
+	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgconn"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
@@ -34,8 +35,8 @@ func (r *TaskRepository) CreateTask(ctx context.Context, title string, user_id i
 				return model.Task{}, apperrors.ErrForeignKey
 			}
 			if pgErr.Code == "23502" {
-				log.Println(apperrors.ErrForeignKey)
-				return model.Task{}, apperrors.ErrForeignKey
+				log.Println(apperrors.ErrNullViolation)
+				return model.Task{}, apperrors.ErrNullViolation
 			}
 		}
 		log.Println(err)
@@ -70,8 +71,18 @@ func (r *TaskRepository) ListTasks(ctx context.Context) ([]model.Task, error) {
 
 func (r *TaskRepository) GetTaskByID(ctx context.Context, id int) (model.Task, error) {
 	var task model.Task
-	err := r.pool.QueryRow(ctx, `SELECT * FROM tasks WHERE id = $1 RETURNING *`, id).Scan(&task.ID, &task.Title, &task.Completed, &task.Created_at, &task.User_id)
+	err := r.pool.QueryRow(ctx, `SELECT * FROM tasks WHERE id = $1`, id).Scan(&task.ID, &task.Title, &task.Completed, &task.Created_at, &task.User_id)
+	// Правило обработки ошибок с использованием QueryRow. Конкретно ошибка NotFound.
+	// Если мы используем QueryRow и запрос с RETURNING (Не распространяется на SELECT,
+	// так как он уже и так возвращает данные) - нам стоит обработать ошибку через
+	// pgx.ErrNoRows. В случае если мы используем Exec (Returning тут уже не работает) -
+	// обрабатываем ошибку через cmdTag.RowsAffected
 	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			log.Println("Task not found")
+			return model.Task{}, apperrors.ErrNotFound
+		}
+		log.Println(err)
 		return model.Task{}, err
 	}
 	return task, nil
@@ -81,6 +92,10 @@ func (r *TaskRepository) UpdateTaskStatus(ctx context.Context, id int, completed
 	var task model.Task
 	err := r.pool.QueryRow(ctx, `UPDATE tasks SET completed = $1 WHERE id=$2 RETURNING id, title, completed, user_id`, completed, id).Scan(&task.ID, &task.Title, &task.Completed, &task.User_id)
 	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			log.Println("Task not found")
+			return model.Task{}, apperrors.ErrNotFound
+		}
 		var pgErr *pgconn.PgError
 		if errors.As(err, &pgErr) {
 			if pgErr.Code == "23502" {
@@ -96,10 +111,145 @@ func (r *TaskRepository) UpdateTaskStatus(ctx context.Context, id int, completed
 
 func (r *TaskRepository) DeleteTask(ctx context.Context, id int) (model.Task, error) {
 	var task model.Task
-	err := r.pool.QueryRow(ctx, `DELETE * FROM tasks WHERE id =$1 RETURNING *`, id).Scan(&task.ID, &task.Title, &task.Completed, &task.Created_at, &task.User_id)
+	err := r.pool.QueryRow(ctx, `DELETE FROM tasks WHERE id =$1 RETURNING *`, id).Scan(&task.ID, &task.Title, &task.Completed, &task.Created_at, &task.User_id)
 	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			log.Println("Task not found")
+			return model.Task{}, apperrors.ErrNotFound
+		}
 		log.Println(err)
 		return model.Task{}, err
 	}
 	return task, err
+}
+
+func (r *UserRepository) CreateUser(ctx context.Context, email string) (model.User, error) {
+	var u model.User
+	err := r.pool.QueryRow(ctx, `INSERT INTO users(email) VALUES($1) RETURNING *`, email).Scan(&u.ID, &u.Email, &u.Created_at)
+	if err != nil {
+		// нет ошибки с несуществующим пользователем так как мы его вставляем
+		var pgErr *pgconn.PgError
+		if errors.As(err, &pgErr) {
+			if pgErr.Code == "23502" {
+				log.Println(apperrors.ErrNullViolation)
+				return model.User{}, apperrors.ErrNullViolation
+			}
+			if pgErr.Code == "23505" {
+				log.Println(apperrors.ErrUsedEmail)
+				return model.User{}, apperrors.ErrUsedEmail
+			}
+		}
+		log.Println(err)
+		return model.User{}, err
+	}
+	return u, nil
+}
+
+func (r *UserRepository) ListUsers(ctx context.Context) ([]model.User, error) {
+	var users []model.User
+	rows, err := r.pool.Query(ctx, `SELECT id, email FROM users`)
+	if err != nil {
+		log.Println(err)
+		return users, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var u model.User
+		err := rows.Scan(&u.ID, &u.Email)
+		if err != nil {
+			log.Println(err)
+			return users, err
+		}
+		users = append(users, u)
+	}
+
+	if err := rows.Err(); err != nil {
+		log.Println(err)
+		return users, err
+	}
+
+	return users, nil
+}
+
+func (r *UserRepository) GetUserByID(ctx context.Context, id int) (model.User, error) {
+	var u model.User
+	err := r.pool.QueryRow(ctx, `SELECT id, email FROM users WHERE id=$1`, id).Scan(&u.ID, &u.Email)
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			log.Println("User not found")
+			return model.User{}, apperrors.ErrNotFound
+		}
+		log.Println(err)
+		return model.User{}, err
+	}
+	return u, nil
+}
+
+func (r *UserRepository) UpdateUser(ctx context.Context, email string, id int) (model.User, error) {
+	var u model.User
+	err := r.pool.QueryRow(ctx, `UPDATE users SET email = $1 WHERE id = $2 RETURNING *`, email, id).Scan(&u.ID, &u.Email, &u.Created_at)
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			log.Println("User not found")
+			return model.User{}, apperrors.ErrNotFound
+		}
+		var pgErr *pgconn.PgError
+		if errors.As(err, &pgErr) {
+			if pgErr.Code == "23502" {
+				log.Println(apperrors.ErrNullViolation)
+				return model.User{}, apperrors.ErrNullViolation
+			}
+			if pgErr.Code == "23505" {
+				log.Println(apperrors.ErrUsedEmail)
+				return model.User{}, apperrors.ErrUsedEmail
+			}
+		}
+		log.Println(err)
+		return model.User{}, err
+	}
+
+	return u, nil
+}
+
+func (r *UserRepository) DeleteUser(ctx context.Context, id int) (model.User, error) {
+	// здесь можно продемонстрировать работу с транзакцией, потому что для удаления
+	// юзера, нужно удалить сначала все его задачи
+
+	// открывает транзакцию
+	tx, err := r.pool.Begin(ctx)
+	if err != nil {
+		log.Println(err)
+		return model.User{}, err
+	}
+	defer tx.Rollback(ctx)
+	// удаляем задачи
+	cmdTag, err := tx.Exec(ctx, `DELETE FROM tasks WHERE user_id = $1`, id) // принимает cmdTag (узнай подробнее что это) (UPD: возвращает что-то типа DELETE 0 1 как в консоли с SQL)
+	if err != nil {
+		log.Println(err)
+		return model.User{}, err
+	}
+	// ошибка отсутствия изменений в Exec обрабатывается через cmdTag
+	if cmdTag.RowsAffected() == 0 {
+		log.Println("No tasks")
+		// не прерываем, потому что у пользователя действительно может не быть задач
+	}
+	var u model.User
+	err = tx.QueryRow(ctx, `DELETE * FROM users WHERE id = $1 RETURNING *`, id).Scan(&u.ID, &u.Email, &u.Created_at)
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			log.Println("User not found:", err)
+			return model.User{}, err
+		}
+		var pgErr *pgconn.PgError
+		if errors.As(err, &pgErr) {
+			if pgErr.Code == "23503" {
+				log.Println(apperrors.ErrForeignKey)
+				return model.User{}, apperrors.ErrForeignKey
+			}
+		}
+		log.Println(err)
+		return model.User{}, err
+	}
+	return u, tx.Commit(ctx)
 }
